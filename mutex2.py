15735ef941d9a70382cf3b30677c954ce7cf9662
6q5so95 <103797420+6q5so95@users.noreply.github.com>
2023-12-02 13:07:15 +0900

    Update mutex2.py

--- mutex2.py
+++ mutex2.py
@@ -13,15 +13,13 @@ class MutexManager:
         except win32api.error as e:
             if e.winerror == 5:  # ERROR_ACCESS_DENIED
                 print("Mutexが既に取得されています。")
-                return False
+                return None
             else:
                 raise
 
         # ここに必要な処理を書く
 
-        # Mutexを解放する
-        cls.release_lock(mutex_handle, caller_package)
-        return True
+        return mutex_handle
 
     @classmethod
     def release_lock(cls, mutex_handle, caller_package):
@@ -33,15 +31,17 @@ class MutexManager:
 # 利用例
 if __name__ == "__main__":
     # ロックが取得できない場合の処理
-    if not MutexManager.acquire_lock(__package__):
+    mutex_handle = MutexManager.acquire_lock(__package__)
+    if not mutex_handle:
         # ロックが既に取得されていた場合の処理
         print("プログラムが既に実行中です.")
     else:
         # ロックが取得できた場合の処理
         print("Mutexが取得されました.")
 
-        # ここにアプリコードを書く
-
-        # Mutexを解放する
-        MutexManager.release_lock(None, __package__)
-
+        try:
+            # ここにアプリコードを書く
+            pass
+        finally:
+            # Mutexを解放する
+            MutexManager.release_lock(mutex_handle, __package__)
